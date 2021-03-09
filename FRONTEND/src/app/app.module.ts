c7b532e941fcf2af40c674c8dd90a991856b84d5
unknown <you@example.comcd>
2021-03-09 17:39:17 +0100

    homepage finished- DONE

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './Components/Shared/Header/header.component';
+import { HeaderComponent } from './Components/Shared/Header/header.component'
 import { AsideComponent } from './Components/Shared/aside/aside.component'
+import { HomepageComponent } from './Components/Main/homepage/homepage.component'
+import { NgParticlesModule } from 'ng-particles'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AsideComponent
+    AsideComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
